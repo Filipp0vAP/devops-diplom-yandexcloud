f99655c86386a003f1b9943071824e130b2e4ce1
Aleksandr Filippov <Filipp0vAP@protonmail.com>
2024-02-18 23:10:25 +0300

    terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,25 +2,25 @@ resource "yandex_kubernetes_cluster" "regional_cluster_resource_name" {
   name        = "name"
   description = "description"
 
-  network_id = "${yandex_vpc_network.network-1.id}"
+  network_id = yandex_vpc_network.network-1.id
 
   master {
     regional {
       region = "ru-central1"
 
       location {
-        zone      = "${yandex_vpc_subnet.prod.zone}"
-        subnet_id = "${yandex_vpc_subnet.prod.id}"
+        zone      = yandex_vpc_subnet.prod.zone
+        subnet_id = yandex_vpc_subnet.prod.id
       }
 
       location {
-        zone      = "${yandex_vpc_subnet.preprod.zone}"
-        subnet_id = "${yandex_vpc_subnet.preprod.id}"
+        zone      = yandex_vpc_subnet.preprod.zone
+        subnet_id = yandex_vpc_subnet.preprod.id
       }
 
       location {
-        zone      = "${yandex_vpc_subnet.dev.zone}"
-        subnet_id = "${yandex_vpc_subnet.dev.id}"
+        zone      = yandex_vpc_subnet.dev.zone
+        subnet_id = yandex_vpc_subnet.dev.id
       }
     }
 
@@ -44,17 +44,17 @@ resource "yandex_kubernetes_cluster" "regional_cluster_resource_name" {
     # }
 
     master_logging {
-      enabled = true
-      folder_id = "${yandex_folder_id}"
-      kube_apiserver_enabled = true
+      enabled                    = true
+      folder_id                  = var.yandex_folder_id
+      kube_apiserver_enabled     = true
       cluster_autoscaler_enabled = true
-      events_enabled = true
-      audit_enabled = true
+      events_enabled             = true
+      audit_enabled              = true
     }
   }
 
-  service_account_id      = "${yandex_iam_service_account.k8s-sa.id}"
-  node_service_account_id = "${yandex_iam_service_account.k8s-node-sa.id}"
+  service_account_id      = yandex_iam_service_account.k8s-sa.id
+  node_service_account_id = yandex_iam_service_account.k8s-node-sa.id
 
   # labels = {
   #   my_key       = "my_value"
